5655ffc2beb3a045dee2d738cfdef2aed4850f7c
sboursault <sebastien.boursault@gmail.com>
2017-09-18 21:17:02 +0200

    improve script to run the environment

--- gs-springboot-rest-mongo-docker/run_in_docker.sh
+++ gs-springboot-rest-mongo-docker/run_in_docker.sh
@@ -1,12 +1,36 @@
 #!/usr/bin/env bash
 
+# building the jar could be in staged docker,
+# but it would not reuse the local gradle repository.
 
-docker-compose down \
-  && gradle clean build -x test \
-  && docker-compose up --build
+USAGE="$(basename "$0") [-h] [-m <message>] [SERVICE...] -- program to launch the asylum environment.
+where:
+    SERVICE... Are the docker services to restart.
+    -h           Shows this help."
 
+while getopts ":hm:" opt; do
+  case $opt in
+    h)
+      echo "$USAGE"
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
 
-# docker down to stop and remove containers
-# check also docker down --volumes
+shift $((OPTIND-1)) # shift all processed options away from parameters (keep only operands)
+SERVICES=$*
 
-# watch the size of /var/lib/mongodb
+if [[ " $@ " =~ " api " ]] || [ $# = 0 ]; then
+  # devrait etre lancé aussi quand $# == 0
+  echo ""
+  echo "===[ building app ]==="
+  (set -x; gradle clean build -x test) # use a subshell and print command (https://stackoverflow.com/a/31392037)
+fi
+
+echo ""
+echo "===[ run docker ]==="
+(set -x; docker-compose up --build $SERVICES)
--- gs-springboot-rest-mongo-docker/src/main/java/gs/AsylumApplication.java
+++ gs-springboot-rest-mongo-docker/src/main/java/gs/AsylumApplication.java
@@ -1,10 +1,17 @@
 package gs;
 
+import com.google.common.collect.Lists;
+import gs.model.Aka;
+import gs.model.Inmate;
+import gs.repository.InmateRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
 
 /*
  * Sprint boot appplication
@@ -20,4 +27,32 @@ public class AsylumApplication {
     public static void main(String[] args) {
         SpringApplication.run(AsylumApplication.class, args);
     }
+
+
+    @Bean
+    CommandLineRunner init(InmateRepository inmateRepository) {
+        // load basic inmates in not existing
+        return args -> {
+            inmateRepository.save(Inmate.builder()
+                    .id("penguin_1234")
+                    .firstname("Oswald")
+                    .lastname("Cobblepot")
+                    .birthDate(LocalDate.of(1960, 05, 31))
+                    .aka(Lists.newArrayList(Aka.builder().name("Penguin").build()))
+                    .build());
+            inmateRepository.save(Inmate.builder()
+                    .id("joker_5555")
+                    .firstname("???")
+                    .lastname("???")
+                    .aka(Lists.newArrayList(Aka.builder().name("Joker").build()))
+                    .build());
+            inmateRepository.save(Inmate.builder()
+                    .id("poisonIvy_7777")
+                    .firstname("Pamela")
+                    .lastname("Isley")
+                    .aka(Lists.newArrayList(Aka.builder().name("Poison Ivy").build()))
+                    .build());
+        };
+    }
+
 }
